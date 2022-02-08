4b87f823c51546a57fed9be0d12c4f2c9bcccb1f
Rômulo Saraiva <romulopsaraiva@gmail.com>
2022-02-08 20:40:29 -0300

    Adicionar modificador de declínio

--- gilded_rose.py
+++ gilded_rose.py
@@ -7,20 +7,29 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
+
+            modifier = 1
+            
+            if item.name.startswith('Conjured'):
+                modifier = 1*modifier
+            else:
+                modifier = 1*modifier
+
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                        item.quality = item.quality - (1*modifier)
             else:
                 if item.quality < 50:
-                    item.quality = item.quality + 1
+                    item.quality = item.quality + (1*modifier)
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
                         if item.sell_in < 11:
                             if item.quality < 50:
-                                item.quality = item.quality + 1
+                                item.quality = item.quality + (1*modifier)
                         if item.sell_in < 6:
                             if item.quality < 50:
-                                item.quality = item.quality + 1
+                                item.quality = item.quality + (1*modifier)
+            
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
@@ -28,12 +37,12 @@ class GildedRose(object):
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
                         if item.quality > 0:
                             if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                                item.quality = item.quality - (1*modifier)
                     else:
                         item.quality = item.quality - item.quality
                 else:
                     if item.quality < 50:
-                        item.quality = item.quality + 1
+                        item.quality = item.quality + (1*modifier)
 
 
 class Item:
