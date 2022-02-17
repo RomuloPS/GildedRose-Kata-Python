b6d8fb0b0012b56960260557427f64793aa61a8a
Rômulo Saraiva <romulopsaraiva@gmail.com>
2022-02-17 18:29:12 -0300

    Criar método update_quality_item

--- gilded_rose.py
+++ gilded_rose.py
@@ -10,35 +10,43 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
-    def update_quality(self):
-        for item in self.items:
-            if item.name != self.AGED_BRIE and item.name != self.BACKSTAGE_PASS:
-                if item.quality > 0:
-                    if item.name != self.SULFURAS:
-                        item.quality = item.quality - 1
+
+    def update_quality_item(self, item):
+        if item.name != self.AGED_BRIE and item.name != self.BACKSTAGE_PASS:
+            if item.quality > 0:
+                if item.name != self.SULFURAS:
+                    item.quality = item.quality - 1
+        else:
+            if item.quality < 50:
+                item.quality = item.quality + 1
+                if item.name == self.BACKSTAGE_PASS:
+                    if item.sell_in < 11:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+                    if item.sell_in < 6:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+        if item.name != self.SULFURAS:
+            item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            if item.name != self.AGED_BRIE:
+                if item.name != self.BACKSTAGE_PASS:
+                    if item.quality > 0:
+                        if item.name != self.SULFURAS:
+                            item.quality = item.quality - 1
+                else:
+                    item.quality = item.quality - item.quality
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == self.BACKSTAGE_PASS:
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != self.SULFURAS:
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != self.AGED_BRIE:
-                    if item.name != self.BACKSTAGE_PASS:
-                        if item.quality > 0:
-                            if item.name != self.SULFURAS:
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+        
+        pass
+
+
+    def update_quality(self):
+        for item in self.items:
+            self.update_quality_item(item)
+
 
 
 class Item:
